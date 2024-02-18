a356874ff41abd361cca2d98b4edb378c91d7c36
Md Farhan Ishtiaque <mdfarhanishtiaque@Mds-MacBook-Pro-2.local>
2024-02-19 02:34:48 +0600

    product list page shimmer added

--- lib/features/product/screens/product_list.dart
+++ lib/features/product/screens/product_list.dart
@@ -6,6 +6,7 @@ import 'package:tr_task/core/constants/app_colors.dart';
 import 'package:tr_task/core/constants/text_styles.dart';
 import 'package:tr_task/features/product/controller/product_list_controller.dart';
 import 'package:tr_task/features/product/screens/widgets/product_card.dart';
+import 'package:tr_task/features/product/screens/widgets/product_card_shimmer.dart';
 
 class ProductList extends StatelessWidget {
   const ProductList({Key? key}) : super(key: key);
@@ -28,7 +29,16 @@ class ProductList extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
         child: Obx(
           () => productListController.isLoading.value
-              ? const CircularProgressIndicator()
+              ? GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: .72,
+                      crossAxisSpacing: 12,
+                      mainAxisSpacing: 8),
+                  itemCount: 6,
+                  itemBuilder: (BuildContext context, int index) {
+                    return const ProductCardShimmer();
+                  })
               : GridView.builder(
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 2,
@@ -41,7 +51,9 @@ class ProductList extends StatelessWidget {
                       avatarUrl:
                           productListController.productList[index].thumbnail,
                       price: productListController.productList[index].id
-                          .toString(), productName: productListController.productList[index].title,
+                          .toString(),
+                      productName:
+                          productListController.productList[index].title,
                     );
                   }),
         ),
